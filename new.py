aaf0e850c830f7948418c328976c8c7e72e0f3bd
Sreeram Aj <sreeramzeno@gmail.com>
2021-08-01 23:48:50 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,10 @@
-from telegram import *
-from telegram.ext import *
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from flask import Flask
+import json, time
 
+app = Flask(__name__)
 
 cred = credentials.Certificate("firebase-sdk.json")
 firebase_admin.initialize_app(cred,{
@@ -22,155 +23,12 @@ Rain_Value = db.reference('Rain_Value')
 Smoke = db.reference('Smoke')
 Temperature = db.reference('Temperature')
 
-bot = Bot("1940119944:AAHP9d3XQyMML5ITRsPTwLj5PDno6VWKEAQ")
-#print(bot.get_me())
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set = {'Air_Quality': Air_Quality.get(), 'Altitude': Altitude.get(), 'Cng': Cng.get(), 'Humidity': Humidity.get(), 'Ldr': Ldr.get(), 'Lpg': Lpg.get(), 'Pressure': Pressure.get(), 'Rain_Value': Rain_Value.get(), 'Smoke': Smoke.get(), 'Temperature': Temperature.get(), 'timestamp': time.time()}
+    json_dump = json.dumps(data_set)
 
-updater=Updater("1940119944:AAHP9d3XQyMML5ITRsPTwLj5PDno6VWKEAQ",use_context=True)
-
-dispatcher=updater.dispatcher
-
-def test_function(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text='''  WEATHER STATION DATA 
-       Welcome to ZenoModiff Home Weather Station. 
-        Use the following commands for stats:-
-       1. /STATUS - Gives all values
-       2. /AQI - Air Quality in PPM
-       3. /ALTI - Altitude in PPM
-       4. /CNG - Compressed Gas in PPM
-       5. /HUMI - Humidity in Percentage
-       6. /LDR - Light Intensity
-       7. /LPG - LPG in PPM
-       8. /PRESS - Pressure Value IN PPM
-       9. /RVLE - Rain Value ,
-       10. /SMKE - Smoke in in PPM,
-       11. /TEMP - Temperature in in °C, '''
-   )
-start_value=CommandHandler('start',test_function)
-
-dispatcher.add_handler(start_value)
-
-def test_function1(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Air_Quality.get() 
-   )
-start_value1=CommandHandler('AQI',test_function1)
-
-dispatcher.add_handler(start_value1)
-
-
-def test_function2(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Altitude.get()
-   )
-start_value2=CommandHandler('ALTI',test_function2)
-
-dispatcher.add_handler(start_value2)
-
-def test_function3(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Cng.get()
-   )
-start_value3=CommandHandler('CNG',test_function3)
-
-dispatcher.add_handler(start_value3)
-
-
-def test_function4(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Humidity.get()
-   )
-start_value4=CommandHandler('HUMI',test_function4)
-
-dispatcher.add_handler(start_value4)
-
-def test_function5(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Ldr.get()
-   )
-start_value5=CommandHandler('LDR',test_function5)
-
-dispatcher.add_handler(start_value5)
-
-
-def test_function6(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Lpg.get()
-   )
-start_value6=CommandHandler('LPG',test_function6)
-
-dispatcher.add_handler(start_value6)
-
-def test_function7(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Pressure.get()
-   )
-start_value7=CommandHandler('PRESS',test_function7)
-
-dispatcher.add_handler(start_value7)
-
-
-def test_function8(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Rain_Value.get()
-   )
-start_value8=CommandHandler('RVLE',test_function8)
-
-dispatcher.add_handler(start_value8)
-
-
-def test_function9(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Smoke.get()
-   )
-start_value9=CommandHandler('SMKE',test_function9)
-
-dispatcher.add_handler(start_value9)
-
-
-def test_function10(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text= Temperature.get()
-   )
-start_value10=CommandHandler('TEMP',test_function10)
-
-dispatcher.add_handler(start_value10)
-
-def test_function11(update:Update,context:CallbackContext):
-    bot.send_message(
-       chat_id=update.effective_chat.id,
-
-       text = [Air_Quality.get(), Altitude.get(), Cng.get(), Humidity.get(), Ldr.get(), Lpg.get(), Pressure.get(), Rain_Value.get(), Smoke.get(), Temperature.get() ]
-
-   )
-start_value11=CommandHandler('STATUS',test_function11)
-
-dispatcher.add_handler(start_value11)
-
-
-updater.start_polling()
-
- # {'Air_Quality': Air_Quality.get(), 'Altitude': Altitude.get(), 'Cng': Cng.get(), 'Humidity': Humidity.get(), 'Ldr': Ldr.get(), 'Lpg': Lpg.get(), 'Pressure': Pressure.get(), 'Rain_Value': Rain_Value.get(), 'Smoke': Smoke.get(), 'Temperature': Temperature.get()}
+    return json_dump
+if __name__ == '__main__':
+    app.run(port=5000)
 
