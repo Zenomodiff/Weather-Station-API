7b3a81e98ce227938fed18855798959e28609f0a
Sashwat K <sashwat0001@gmail.com>
2021-10-16 10:54:51 +0530

    Added Dockerfile

--- app.py
+++ app.py
@@ -2,33 +2,49 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from flask import Flask
-import json, time
+import json
+import time
 
 app = Flask(__name__)
-
 cred = credentials.Certificate("firebase-sdk.json")
-firebase_admin.initialize_app(cred,{
+firebase_admin.initialize_app(
+    cred, {
+        "databaseURL":
+        "https://weather-station-91122-default-rtdb.firebaseio.com/"
+    })
+air_quality = db.reference("Air_Quality")
+altitude = db.reference("Altitude")
+cng = db.reference("Cng")
+humidity = db.reference("Humidity")
+ldr = db.reference("Ldr")
+lpg = db.reference("Lpg")
+pressure = db.reference("Pressure")
+rain_value = db.reference("Rain_Value")
+smoke = db.reference("Smoke")
+temperature = db.reference("Temperature")
 
-    'databaseURL': 'https://weather-station-91122-default-rtdb.firebaseio.com/'
-})
-
-Air_Quality = db.reference('Air_Quality')
-Altitude = db.reference('Altitude')
-Cng = db.reference('Cng')
-Humidity = db.reference('Humidity')
-Ldr = db.reference('Ldr')
-Lpg = db.reference('Lpg')
-Pressure = db.reference('Pressure')
-Rain_Value = db.reference('Rain_Value')
-Smoke = db.reference('Smoke')
-Temperature = db.reference('Temperature')
 
 @app.route('/', methods=['GET'])
 def home_page():
-    data_set = {'Air_Quality': Air_Quality.get(), 'Altitude': Altitude.get(), 'Cng': Cng.get(), 'Humidity': Humidity.get(), 'Ldr': Ldr.get(), 'Lpg': Lpg.get(), 'Pressure': Pressure.get(), 'Rain_Value': Rain_Value.get(), 'Smoke': Smoke.get(), 'Temperature': Temperature.get(), 'timestamp': time.time()}
+    data_set = {
+        'Air_Quality': air_quality.get(),
+        'Altitude': altitude.get(),
+        'Cng': cng.get(),
+        'Humidity': humidity.get(),
+        'Ldr': ldr.get(),
+        'Lpg': lpg.get(),
+        'Pressure': pressure.get(),
+        'Rain_Value': rain_value.get(),
+        'Smoke': smoke.get(),
+        'Temperature': temperature.get(),
+        'timestamp': time.time()}
     json_dump = json.dumps(data_set)
-
     return json_dump
-if __name__ == '__main__':
+
+
+def main():
     app.run(port=5000)
 
+
+if __name__ == '__main__':
+    main()
