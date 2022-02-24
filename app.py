a334e7ff7d333b1bbb66c75ce27514050e991f8d
Sreeram Aj <sreeramzeno@gmail.com>
2022-02-24 10:37:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,42 @@ firebase_admin.initialize_app(cred, {
 
 @app.route('/', methods=['GET'])
 def home_page():
-    data_set = {"Air_Quality" : db.reference("Air_Quality").get(), "Altitude" : db.reference("Altitude").get(),  "Cng": db.reference("Cng").get(), 
-                                                "Humidity" : db.reference("Humidity").get(), "Ldr" : db.reference("Ldr").get(),  "Lpg" : db.reference("Lpg").get(), 
-                                                "Pressure" : db.reference("Pressure").get(), "Rain_Value" : db.reference("Rain_Value").get(), 
-                                                "Smoke": db.reference("Smoke").get(),
-                                                "Temperature" : db.reference("Temperature").get()}
-    json_dump = json.dumps(data_set) 
 
-    return json_dump
-    
+    Air_Quality = db.reference("Air_Quality").get()
+    Altitude = db.reference("Altitude").get()
+    Cng = db.reference("Cng").get()
+    Humidity = db.reference("Humidity").get()
+    Ldr = db.reference("Ldr").get()
+    Lpg = db.reference("Lpg").get()
+    Pressure = db.reference("Pressure").get()
+    Rain_Value = db.reference("Rain_Value").get()
+    Smoke = db.reference("Smoke").get()
+    Temperature = db.reference("Temperature").get()
+
+    def main():
+       (Air_Quality,Altitude,Cng,Humidity,Ldr,Lpg,Pressure,Rain_Value,Smoke,Temperature)
+    data = {}
+    data = {
+
+        "Air_Quality":Air_Quality, 
+        "Altitude":Altitude,
+        "Cng":Cng, 
+        "Humidity":Humidity,
+        "Ldr":Ldr, 
+        "Lpg":Lpg,
+        "Pressure":Pressure, 
+        "Rain_Value":Rain_Value,
+        "Smoke":Smoke, 
+        "Temperature":Temperature
+        
+    }
+    New_Data = str(json.dumps(data))
+    main()
+    with open("data.json", "w") as file:
+       file.write(f"[{New_Data}]")
+       file.write("\n") 
+       json_dump = data
+       return json_dump
+
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port= PORT)
